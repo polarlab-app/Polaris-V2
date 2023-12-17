2f2b20cf0aa324524831d7e8a42e51c65d6ca5d1
Aertic <63727339+Aertic@users.noreply.github.com>
2023-12-17 15:44:30 +0100

    Added Data

---
+++ data/consoleColors.js
@@ -0,0 +1,9 @@
+module.exports = {
+    colors: {
+        debug: "\x1b[33m",
+        error: "\x1b[31m",
+        success: "\x1b[32m",
+        info: "\x1b[36m",
+        regular: "\x1b[30m"
+    }
+}
