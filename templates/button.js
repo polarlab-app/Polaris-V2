21f0bd833ac3cd75466739f3d4847d8aba78a003
Aertic <63727339+Aertic@users.noreply.github.com>
2023-12-17 21:44:08 +0100

    Updated templates to be more lightweight and optimized

--- templates/button.js
+++ templates/button.js
@@ -1,6 +1,3 @@
-const { modules } = require('../../config.json');
-const { colors } = require('../../data/colors');
-
 const errorHandler = require('../../handlers/errorHandler');
 const consoleLogHandler = require('../../handlers/consoleLogHandler');
 
@@ -11,38 +8,20 @@ module.exports = {
     botPermissions: [],
 
     callback: async (polaris, interaction) => {
-        await interaction.deferReply();
-        if (modules.module === 'disabled') {
-            try {
-                await errorHandler({
-                    interaction: interaction,
-                    errorType: 'systemDisabled',
-                    commandName: module.exports.name,
-                });
-            } catch (error) {
-                await errorHandler({
-                    interaction: interaction,
-                    errorType: 'generic',
-                    commandName: module.exports.name,
-                    error: error,
-                });
-            }
-        } else {
-            try {
-                interaction.editReply('test');
-                await consoleLogHandler({
-                    interaction: interaction,
-                    commandName: module.exports.name,
-                    errorType: 'commandRan',
-                });
-            } catch (error) {
-                await errorHandler({
-                    interaction: interaction,
-                    errorType: 'generic',
-                    commandName: module.exports.name,
-                    error: error,
-                });
-            }
+        try {
+            interaction.editReply('test');
+            await consoleLogHandler({
+                interaction: interaction,
+                commandName: module.exports.name,
+                errorType: 'commandRan',
+            });
+        } catch (error) {
+            await errorHandler({
+                interaction: interaction,
+                errorType: 'generic',
+                commandName: module.exports.name,
+                error: error,
+            });
         }
     },
 };
--- templates/command.js
+++ templates/command.js
@@ -1,34 +1,15 @@
-const { modules } = require('../../config.json');
-const { colors } = require('../../data/colors');
-
 const errorHandler = require('../../handlers/errorHandler');
 const consoleLogHandler = require('../../handlers/consoleLogHandler');
 
 module.exports = {
     name: '',
     description: '',
+    module: '',
     
     permissionsRequired: [],
     botPermissions: [],
 
     callback: async (polaris, interaction) => {
-        await interaction.deferReply();
-        if (modules.module === 'disabled') {
-            try {
-                await errorHandler({
-                    interaction: interaction,
-                    errorType: 'systemDisabled',
-                    commandName: module.exports.name,
-                });
-            } catch (error) {
-                await errorHandler({
-                    interaction: interaction,
-                    errorType: 'generic',
-                    commandName: module.exports.name,
-                    error: error,
-                });
-            }
-        } else {
             try {
                 interaction.editReply('test');
                 await consoleLogHandler({
@@ -44,6 +25,5 @@ module.exports = {
                     error: error,
                 });
             }
-        }
-    },
+        },
 };
