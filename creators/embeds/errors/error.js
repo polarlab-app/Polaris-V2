648d3d70bc7a7849bb3a5c495b61bb5d84535af2
Aertic <63727339+Aertic@users.noreply.github.com>
2023-12-17 17:22:04 +0100

    Added error Embed

---
+++ creators/embeds/errors/error.js
@@ -0,0 +1,13 @@
+const { EmbedBuilder } = require("discord.js");
+const { embeds } = require("../../../data/errorMessages.json")
+
+function errorEmbedCreator(errorType) {
+    const embedContent = embeds[errorType];
+    const errorEmbed = new EmbedBuilder()
+    .setColor("#2B2D31")
+    .setTitle(embedContent)
+
+    return errorEmbed;
+}
+
+module.exports = errorEmbedCreator;
