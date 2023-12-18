017d6303dab937c15419ca240a3475b48150b3e0
Aertic <63727339+Aertic@users.noreply.github.com>
2023-12-18 21:33:21 +0100

    Added errors to embedData, optimized builders

--- creators/embeds/errorBuilder.js
+++ creators/embeds/errorBuilder.js
@@ -1,11 +1,10 @@
 const { EmbedBuilder } = require("discord.js");
-const { embeds } = require("../../data/errorMessages.json")
+const { errors } = require("../../data/embedData.json")
 
 function errorEmbedCreator(errorType) {
-    const embedContent = embeds[errorType];
     const errorEmbed = new EmbedBuilder()
         .setColor("#2B2D31")
-        .setTitle(embedContent)
+        .setTitle(errors[errorType])
 
     return errorEmbed;
 }
