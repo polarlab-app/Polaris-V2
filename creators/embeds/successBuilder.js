96ec98f1fff4792b54cab292e224120f97ced517
Aertic <63727339+Aertic@users.noreply.github.com>
2023-12-22 21:17:07 +0100

    Moved replacer to a seperate file, added volume editor, modified embed Builders

--- creators/embeds/successBuilder.js
+++ creators/embeds/successBuilder.js
@@ -1,12 +1,20 @@
 const { EmbedBuilder } = require('discord.js');
 const { success } = require('../../data/embedData.json');
+const replaceArray = require('../../utilities/replaceArray');
 
 async function successEmbedBuilder(embedType, props) {
-    const successEmbed = new EmbedBuilder()
-    .setColor('#2B2D31')
-    .setTitle(success[embedType]);
-
-    return successEmbed;
+    if (props) {
+        const embedText = success[embedType];
+        const successEmbed = new EmbedBuilder()
+        .setColor('#2B2D31')
+        .setTitle(await replaceArray(embedText, props))
+        return successEmbed
+    } else {
+        const successEmbed = new EmbedBuilder()
+        .setColor('#2B2D31')
+        .setTitle(success[embedType]);
+        return successEmbed
+    }
 }
 
 module.exports = successEmbedBuilder;
