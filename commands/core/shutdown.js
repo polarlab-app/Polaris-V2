c8d039fd784faadc5add35d46c6e90d99c521165
Aertic <63727339+Aertic@users.noreply.github.com>
2023-12-31 16:42:09 +0100

    Added shutdown command

---
+++ commands/core/shutdown.js
@@ -0,0 +1,36 @@
+const errorHandler = require('../../handlers/errorHandler');
+const consoleLogHandler = require('../../handlers/consoleLogHandler');
+const successEmbedBuilder = require('../../creators/embeds/successBuilder');
+
+module.exports = {
+    name: 'shutdown',
+    description: 'Shutdowns the bot',
+    module: 'core',
+    
+    permissionsRequired: [],
+    botPermissions: [],
+    devOnly: true,
+
+    callback: async (polaris, interaction) => {
+        try {
+            const embed = await successEmbedBuilder('shutdown')
+            await interaction.editReply({embeds: [embed]})
+            await consoleLogHandler({
+                interaction: interaction,
+                commandName: module.exports.name,
+                errorType: 'commandRan',
+            });
+            await consoleLogHandler({
+                errorType: 'shutdown',
+            });
+            process.exit()
+        } catch (error) {
+            await errorHandler({
+                interaction: interaction,
+                errorType: 'generic',
+                commandName: module.exports.name,
+                error: error,
+            });
+        }
+    },
+};
