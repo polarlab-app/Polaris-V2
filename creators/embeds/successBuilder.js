d3ac0af131a7fd3cd92a2913c917852220dac82a
Aertic <63727339+Aertic@users.noreply.github.com>
2024-01-02 16:01:58 +0100

    Added Nick command

--- creators/embeds/successBuilder.js
+++ creators/embeds/successBuilder.js
@@ -5,11 +5,15 @@ const replaceArray = require('../../utilities/replaceArray');
 async function successEmbedBuilder(embedType, props) {
     if (props) {
         const embedText = success[embedType];
-        const successEmbed = new EmbedBuilder().setColor('#2B2D31').setTitle(await replaceArray(embedText, props));
-        return successEmbed;
+        const successEmbed = new EmbedBuilder()
+        .setColor('#2B2D31')
+        .setTitle(await replaceArray(embedText, props))
+        return successEmbed
     } else {
-        const successEmbed = new EmbedBuilder().setColor('#2B2D31').setTitle(success[embedType]);
-        return successEmbed;
+        const successEmbed = new EmbedBuilder()
+        .setColor('#2B2D31')
+        .setTitle(success[embedType]);
+        return successEmbed
     }
 }
 
