5e28de0359930bb11531a2bb32a3d7f519308c08
Aertic <63727339+Aertic@users.noreply.github.com>
2024-02-09 09:32:17 +0100

    [BUGFIX] Pingmoji was always red

--- commands/core/ping.js
+++ commands/core/ping.js
@@ -14,18 +14,16 @@ module.exports = {
         try {
             const ping = await polaris.ws.ping;
             let pingmoji;
-            switch (ping) {
-                case ping < 120:
-                    pingmoji = '<:green:1127508369729671229>';
-                    break;
-                case ping < 300:
-                    pingmoji = '<:gold:1163487401574924338>';
-                    break;
-                default:
-                    pingmoji = '<:red:1127513643857227856>';
+
+            if (ping < 120) {
+                pingmoji = '<:green:1127508369729671229>';
+            } else if (ping < 300) {
+                pingmoji = '<:gold:1163487401574924338>';
+            } else {
+                pingmoji = '<:red:1127513643857227856>';
             }
 
-            const embed = await embedBuilder('ping', `${module.exports.module}`, [await polaris.ws.ping, pingmoji]);
+            const embed = await embedBuilder('ping', `${module.exports.module}`, [ping, pingmoji]);
             await interaction.editReply({ embeds: [embed] });
             await consoleLogHandler({
                 interaction: interaction,
