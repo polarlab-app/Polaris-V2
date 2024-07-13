e66b28bc0450691046ff9aaeb2a90021a0680d4e
Aertic <63727339+Aertic@users.noreply.github.com>
2024-07-13 11:01:02 +0200

    Fixed discord js verion in startup, bumb bot versions, add messageLogs to the new format

--- events/messageDelete/messageDelete.js
+++ events/messageDelete/messageDelete.js
@@ -3,24 +3,41 @@ const embedBuilder = require('../../creators/embeds/embedBuilder');
 const { AuditLogEvent } = require('discord.js');
 
 module.exports = async (polaris, message) => {
+  try {
     const guild = await guildData.findOne({ id: message.guild.id });
-    if ((await guild.config.logs.messageLogs.find((cfg) => cfg.name == 'status').value) !== 'enabled') {
-        return;
+    if (!guild) {
+      return;
     }
 
-    let channelSend;
-    if (guild.config.logs.messageLogs.find((cfg) => cfg.name == 'channelId').value != 0) {
-        channelSend = message.guild.channels.cache.find(
-            (c) => c.id == guild.config.logs.messageLogs.find((cfg) => cfg.name == 'channelId').value
+    if (guild.config.logs.messageLogs.status == true) {
+      let channelSend = await message.guild.channels.cache.find(
+        (c) => c.id == guild.config.logs.messageLogs.channelId
+      );
+
+      if (!guild.config.logs.messageLogs.channelId || !channelSend) {
+        channelSend = await message.guild.channels.cache.find(
+          (c) => c.topic == 'pmessagelogs'
         );
-    } else {
-        channelSend = message.guild.channels.cache.find((c) => c.topic == 'pmessagelogs');
+        if (!channelSend) {
+          return;
+        }
+      }
+
+      const auditLogs = await message.guild.fetchAuditLogs({
+        type: AuditLogEvent.MessageDelete,
+        limit: 1,
+      });
+      const messageCreateLog = auditLogs.entries.first();
+      const creator = await messageCreateLog.executor;
+
+      const embed = await embedBuilder('messageDelete', 'logs', [
+        creator.id,
+        message.content,
+        message.author.id,
+      ]);
+      await channelSend.send({ embeds: [embed] });
     }
-
-    const auditLogs = await message.guild.fetchAuditLogs({ type: AuditLogEvent.MessageDelete, limit: 1 });
-    const messageCreateLog = auditLogs.entries.first();
-    const creator = await messageCreateLog.executor;
-
-    const embed = await embedBuilder('messageDelete', 'logs', [creator.id, message.content, message.author.id]);
-    await channelSend.send({ embeds: [embed] });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- events/ready/consoleLog.js
+++ events/ready/consoleLog.js
@@ -1,6 +1,5 @@
-const version = require("discord.js");
-const config = require('../../config.json')
-const { colors } = require('../../data/consoleColors')
+const config = require('../../config.json');
+const { colors } = require('../../data/consoleColors');
 
 module.exports = async (polaris) => {
   const timer = (ms) => new Promise((res) => setTimeout(res, ms));
@@ -9,9 +8,11 @@ module.exports = async (polaris) => {
   console.log(colors.regular + `Logged in as: ${polaris.user.tag}`);
   console.log(colors.regular + `Current Ping: ${polaris.ws.ping}ms`);
   console.log(colors.regular + `Current OS: ${process.platform}`);
-  console.log(colors.regular + `Discord.JS Version: ${version}`);
+  console.log(
+    colors.regular + `Discord.JS Version: ${require('discord.js').version}`
+  );
   console.log(colors.regular + `Polaris Version: ${config.version}`);
-  console.log(colors.regular + `Polar API VersioN: V1.2.6`)
+  console.log(colors.regular + `Polar API Version: V1.2.6`);
   console.log(colors.regular + `Current Prefix: /`);
-  console.log("ᅠᅠ");
+  console.log('ᅠᅠ');
 };
