3054be280972071251471014013fca423c638a73
Aertic <63727339+Aertic@users.noreply.github.com>
2024-09-14 12:15:38 +0200

    add node connection logging

---
+++ events/moonlink.nodeCreate/nodeConneciton.js
@@ -0,0 +1,5 @@
+const { colors } = require('../../data/consoleColors');
+
+module.exports = async (node) => {
+    console.log(colors.success + `[POLARIS STABLE] Music node successfully connected!`);
+};
