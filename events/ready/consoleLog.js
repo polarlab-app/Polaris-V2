b47ae27babbaf728cea03ffbf6c8a8c528369c76
Aertic <63727339+Aertic@users.noreply.github.com>
2024-09-14 12:15:58 +0200

    Remove timeout

--- events/ready/consoleLog.js
+++ events/ready/consoleLog.js
@@ -2,9 +2,6 @@ const config = require('../../config.json');
 const { colors } = require('../../data/consoleColors');
 
 module.exports = async (polaris) => {
-    const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-
-    await timer(400);
     console.log(colors.regular + `Logged in as: ${polaris.user.tag}`);
     console.log(colors.regular + `Current Ping: ${polaris.ws.ping}ms`);
     console.log(colors.regular + `Current OS: ${process.platform}`);
