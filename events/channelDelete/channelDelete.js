c467dcb2dc26142175f1d36766c0a52cdfe38020
Aertic <63727339+Aertic@users.noreply.github.com>
2024-09-14 15:12:09 +0200

    update channel logs to new schema

--- events/channelDelete/channelDelete.js
+++ events/channelDelete/channelDelete.js
@@ -1,32 +1,61 @@
 const guildData = require('../../schemas/guildData');
 const embedBuilder = require('../../creators/embeds/embedBuilder');
 const { AuditLogEvent } = require('discord.js');
+const caseSchema = require('../../schemas/case');
 
 module.exports = async (polaris, channel) => {
     const guild = await guildData.findOne({ id: channel.guildId });
-    console.log(guild.config.logs.channelLogs.status);
     if (!guild) {
         return;
     }
 
     if (guild.config.logs.channelLogs.status) {
-        let channelSend = await channel.guild.channels.cache.find(
-            (c) => c.id == guild.config.logs.channelLogs.channelId
-        );
-
-        if (!guild.config.logs.channelLogs.channelId || !channelSend) {
-            channelSend = await channel.guild.channels.cache.find((c) => c.topic == 'pchannellogs');
-            if (!channelSend) {
-                return;
-            }
-        }
+        let channelSend;
 
         const auditLogs = await channel.guild.fetchAuditLogs({
             type: AuditLogEvent.ChannelDelete,
             limit: 2,
         });
-        const channelCreateLog = auditLogs.entries.first();
+        const channelCreateLog = await auditLogs.entries.first();
         const creator = await channelCreateLog.executor;
+        await caseSchema.create({
+            id: 't',
+            name: 'channelLogs',
+            serverID: channel.guild.id,
+            status: 'Closed',
+            action: 'Channel Deleted',
+            date: new Date().toISOString(),
+            duration: 'Permanent',
+            users: {
+                offenderID: channel.id,
+                offenderUsername: channel.name,
+                authorID: creator.id,
+                authorUsername: creator.username,
+            },
+            details: {
+                note: 'N/A',
+                reason: 'N/A',
+                proof: 'N/A',
+            },
+        });
+
+        if (!guild.config.logs.channelLogs.channelID) {
+            channelSend = await channel.guild.channels.cache.find((c) => c.topic == 'pchannellogs');
+            if (!channelSend) {
+                return;
+            }
+        } else {
+            channelSend = await channel.guild.channels.cache.find(
+                (c) => c.id == guild.config.logs.channelLogs.channelID
+            );
+
+            if (!channelSend) {
+                channelSend = await channel.guild.channels.cache.find((c) => c.topic == 'pchannellogs');
+                if (!channelSend) {
+                    return;
+                }
+            }
+        }
 
         const embed = await embedBuilder('channelDelete', 'logs', [creator.id, channel.name, channel.id]);
         await channelSend.send({ embeds: [embed] });
