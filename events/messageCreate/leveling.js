d8d6edfeb40e22860ace644daa06c3133e57f4b3
Aertic <63727339+Aertic@users.noreply.github.com>
2024-09-14 15:07:57 +0200

    fix and update leveling

--- events/messageCreate/leveling.js
+++ events/messageCreate/leveling.js
@@ -23,60 +23,50 @@ module.exports = async (polaris, message) => {
         });
         return;
     } else {
+        member.exp = parseInt(member.exp);
         if (guild.config.leveling.type == 'static') {
-            member.exp += guild.config.leveling.amount;
+            member.exp += parseInt(guild.config.leveling.amount);
         } else {
-            member.exp += Math.floor(
-                Math.round(
-                    Math.random() *
-                        (guild.config.leveling.amount.split('/')[1] - guild.config.leveling.amount.split('/')[0] + 1) +
-                        guild.config.leveling.amount.split('/')[0]
-                )
-            );
+            const [min, max] = guild.config.leveling.amount.split('/');
+
+            member.exp += Math.floor(Math.random() * (parseInt(max) - parseInt(min) + 1)) + parseInt(min);
         }
         await member.save();
 
         const rankExp = await calculateRankExp(member.rank);
 
         if (member.exp >= rankExp) {
+            member.rank = parseInt(member.rank);
+
             member.rank += 1;
             await member.save();
 
             if (guild.config.leveling.display.status) {
+                const embed = await embedBuilder('leveling', 'leveling', [
+                    parseInt(member.rank),
+                    parseInt(member.rank) - 1,
+                    message.member.id,
+                ]);
+
                 if (guild.config.leveling.display.type == 'dedicated') {
                     if (guild.config.leveling.display.channelID) {
                         const channel = await message.guild.channels.cache.get(guild.config.leveling.display.channelID);
                         if (!channel) return;
-                        const embed = await embedBuilder('leveling', 'leveling', [
-                            member.rank,
-                            member.rank - 1,
-                            message.member.id,
-                        ]);
                         await channel.send({ embeds: [embed] });
                     } else {
                         message.guild.channels.cache.forEach(async (channel) => {
                             if (channel.topic == 'pleveling') {
-                                const embed = await embedBuilder('leveling', 'leveling', [
-                                    member.rank,
-                                    member.rank - 1,
-                                    message.member.id,
-                                ]);
                                 await channel.send({ embeds: [embed] });
                             }
                         });
                     }
                 } else {
-                    const embed = await embedBuilder('leveling', 'leveling', [
-                        member.rank,
-                        member.rank - 1,
-                        message.member.id,
-                    ]);
                     await message.channel.send({ embeds: [embed] });
                 }
             }
-            if (guild.config.leveling.rewards) {
-                const rankRole = guild.config.leveling.rewards.find((reward) => reward.id == member.rank);
-                if (rankRole) {
+            if (guild.config.leveling.rewards.status) {
+                const rankRole = guild.config.leveling.rewards.rewards.find((reward) => reward.id == member.rank);
+                if (rankRole && rankRole.status) {
                     const role = await message.guild.roles.cache.get(rankRole.value);
                     await message.member.roles.add(role);
                     await consoleLogHandler({ errorType: 'rankAdded', message: message });
