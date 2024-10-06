0b8ab2ffddcc0f837847731da7a5132f20c83808
Aertic <63727339+Aertic@users.noreply.github.com>
2024-10-06 11:06:07 +0200

    Add multipliers

--- events/messageCreate/leveling.js
+++ events/messageCreate/leveling.js
@@ -27,11 +27,58 @@ module.exports = async (polaris, message) => {
             });
             return;
         } else {
+            const calculateMultiplier = (boosters, ids) => {
+                return boosters.reduce(
+                    (acc, booster) => {
+                        if (booster.status == true && ids.includes(booster.id)) {
+                            if (guild.config.leveling.boosters.mode == 'multiply') {
+                                return acc * booster.value;
+                            } else if (guild.config.leveling.boosters.mode == 'add') {
+                                console.log(acc + parseInt(booster.value));
+                                return acc + parseInt(booster.value);
+                            } else if (guild.config.leveling.boosters.mode == 'maximum') {
+                                return Math.max(acc, parseInt(booster.value));
+                            }
+                        }
+                        return acc;
+                    },
+                    guild.config.leveling.boosters.mode === 'multiply' ? 1 : 0
+                );
+            };
+
+            let multiplier = 1;
+            if (guild.config.leveling.boosters.status == true) {
+                const roleMultiplier = calculateMultiplier(
+                    guild.config.leveling.boosters.roleBoosters,
+                    message.member.roles.cache.map((role) => role.id)
+                );
+
+                const channelMultiplier = calculateMultiplier(guild.config.leveling.boosters.channelBoosters, [
+                    message.channel.id,
+                ]);
+
+                const memberMultiplier = calculateMultiplier(guild.config.leveling.boosters.memberBoosters, [
+                    message.author.id,
+                ]);
+
+                if (guild.config.leveling.boosters.mode === 'multiply') {
+                    multiplier *= roleMultiplier * channelMultiplier * memberMultiplier;
+                } else if (guild.config.leveling.boosters.mode === 'add') {
+                    multiplier += roleMultiplier + channelMultiplier + memberMultiplier;
+                } else if (guild.config.leveling.boosters.mode === 'maximum') {
+                    multiplier = Math.max(multiplier, roleMultiplier, channelMultiplier, memberMultiplier);
+                }
+            }
+
             if (guild.config.leveling.type == 'static') {
-                member.stats.exp += parseInt(guild.config.leveling.amount);
+                member.stats.exp += parseInt(guild.config.leveling.amount) * multiplier;
             } else {
                 const [min, max] = guild.config.leveling.amount.split('/');
-                member.stats.exp += Math.floor(Math.random() * (parseInt(max) - parseInt(min) + 1)) + parseInt(min);
+                member.stats.exp +=
+                    (Math.floor(Math.random() * (parseInt(max) - parseInt(min) + 1)) + parseInt(min)) * multiplier;
+                console.log(
+                    (Math.floor(Math.random() * (parseInt(max) - parseInt(min) + 1)) + parseInt(min)) * multiplier
+                );
             }
 
             await member.save();
